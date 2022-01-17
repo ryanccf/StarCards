e5ec58318cd2c54389a8966831a65f3d9cb03e2a
Ryan <49543952+ryanccf@users.noreply.github.com>
2022-01-16 20:57:05 -0600

    Add Health Bar
    Add health bar, max health, and current_health.

---
+++ PlayerCharacter.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var maxHP = 10 
+var currentHP = maxHP
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	check_death()
+
+func check_death():
+	if currentHP <= 0:
+		print("The Death of a Player Character. ;'-(")
+		queue_free()
+
+func _on_Poison_timeout():
+	currentHP -= 1
+	update_health_bar_size()
+
+func update_health_bar_size():
+	$HealthBar.rect_size.x = currentHP
