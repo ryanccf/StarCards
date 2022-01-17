537eb53bbaa11451451af054d54252c327e86f17
Ryan <49543952+ryanccf@users.noreply.github.com>
2022-01-16 21:08:55 -0600

    Add Health Bar Label
    Health Bar now has a cool label displaying the remaining HP the character has.

--- PlayerCharacter.gd
+++ PlayerCharacter.gd
@@ -5,7 +5,10 @@ var currentHP = maxHP
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	update_hp()
+
+func update_hp():
+	$HP.text = String(currentHP) + "/" + String(maxHP)	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,9 +19,11 @@ func check_death():
 		print("The Death of a Player Character. ;'-(")
 		queue_free()
 
+# This guy is temporary.
 func _on_Poison_timeout():
 	currentHP -= 1
 	update_health_bar_size()
 
 func update_health_bar_size():
 	$HealthBar.rect_size.x = currentHP
+	update_hp()
