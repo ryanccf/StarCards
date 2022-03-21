684ad1d4111213521f029110ac9babcb00bbe3f9
jcf1991 <35503202+jcf1991@users.noreply.github.com>
2022-03-21 16:40:19 -0700

    Made Warrior and Defender responsible for loading their own images.

--- Defender.gd
+++ Defender.gd
@@ -1,10 +1,12 @@
 extends "res://Monster.gd"
+var defenderGraphic = load("res://images/ship7.png")
 
 func _ready():
 	._ready()
 	maxHP = 100 
 	currentHP = maxHP
 	update_hp()
+	update_graphic(defenderGraphic)
 
 func _check_lasers(delta):
 	pass
--- Warrior.gd
+++ Warrior.gd
@@ -1,2 +1,6 @@
 extends "res://Monster.gd"
+var warriorGraphic = load("res://images/ship.png")
 
+func _ready():
+	._ready()
+	update_graphic(warriorGraphic)
