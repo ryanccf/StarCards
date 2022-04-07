41551e00217b6fc35b8f780f5b615ceb4e135fe0
jcf1991 <35503202+jcf1991@users.noreply.github.com>
2022-04-07 07:25:12 -0500

    Making a full card list globally available.

--- scripts/Battle.gd
+++ scripts/Battle.gd
@@ -103,8 +103,9 @@ func check_battle_end():
 		if weakref(enemy_monster).get_ref() and enemy_monster.monster_type == "base":
 			base_found = true
 	if not base_found:
+		Global.add_card("Archer")
 		get_tree().change_scene("res://Victory.tscn")
-	elif game_over:
+	elif game_over:		
 		get_tree().change_scene("res://Defeat.tscn")
 
 func update_battle_arrays():
--- scripts/Global.gd
+++ scripts/Global.gd
@@ -4,6 +4,7 @@ var Decklist = preload("res://Scenes/Decklist.tscn")
 
 var player_name = "Player 1"
 var decks = []
+var cards = Decklist.instance()
 
 func set_player_name(new_name):
 	player_name = new_name
@@ -14,17 +15,29 @@ func get_player_name():
 func get_decks():
 	return decks
 
+func get_cards():
+	return cards
+
+func add_card(card_name):
+	cards.push_back(card_name)
+
 func initialize():
 	var new_decklist = Decklist.instance()
 	new_decklist.add_card("Warrior")
 	new_decklist.add_card("Defender")
-	new_decklist.add_card("Archer")
+	#new_decklist.add_card("Archer")
 	new_decklist.add_card("Barbarian")
 	new_decklist.add_card("Warrior")
 	new_decklist.add_card("Warrior")
 	new_decklist.add_card("Warrior")
 	decks.push_back(new_decklist)
-	print(decks[0].get_cards())
+	cards.add_card("Warrior")
+	cards.add_card("Defender")
+	#cards.add_card("Archer")
+	cards.add_card("Barbarian")
+	cards.add_card("Warrior")
+	cards.add_card("Warrior")
+	cards.add_card("Warrior")
 
 func _ready():
 	initialize()
