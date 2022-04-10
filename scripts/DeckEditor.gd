b95bac838d806c7ac24e869c0046814901bf7431
jcf1991 <35503202+jcf1991@users.noreply.github.com>
2022-04-10 18:48:15 -0500

    Made a list of things.

--- scripts/DeckEditor.gd
+++ scripts/DeckEditor.gd
@@ -3,7 +3,12 @@ extends Control
 
 func _ready():
 	pass
-
+	
+func _process(delta):
+	print(rect_size.y)
+	$ScrollContainer.add_constant_override("margin_bottom", rect_size.y)
+	$ScrollContainer.add_constant_override("rect_size", rect_size.y)
+	
 
 func _on_ExitArea_input_event(viewport, event, shape_idx):
 	if "button_index" in event and event.button_index == BUTTON_LEFT and event.pressed:
