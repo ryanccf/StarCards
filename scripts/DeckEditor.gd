fa9a46ed621996ad770c1aa09720124b746d61be
jcf1991 <35503202+jcf1991@users.noreply.github.com>
2022-04-10 19:57:47 -0500

    Removed excessive print statement.

--- scripts/DeckEditor.gd
+++ scripts/DeckEditor.gd
@@ -5,10 +5,7 @@ func _ready():
 	pass
 	
 func _process(delta):
-	print(rect_size.y)
 	$ScrollContainer.add_constant_override("margin_bottom", rect_size.y)
-	$ScrollContainer.add_constant_override("rect_size", rect_size.y)
-	
 
 func _on_ExitArea_input_event(viewport, event, shape_idx):
 	if "button_index" in event and event.button_index == BUTTON_LEFT and event.pressed:
