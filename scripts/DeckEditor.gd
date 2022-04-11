85b60bf68913f9627dbc58eee7889cc848a7962e
jcf1991 <35503202+jcf1991@users.noreply.github.com>
2022-04-10 20:08:58 -0500

    Added some code to start iterating over avaiable card in DeckEditor._ready.

--- scripts/DeckEditor.gd
+++ scripts/DeckEditor.gd
@@ -1,9 +1,19 @@
 extends Control
 
-
 func _ready():
-	pass
+	var deck = Global.get_decks()[0].get_cards()
+	var cards = Global.get_cards()
 	
+	var card_count_map = {
+		"Archer" : 0,
+		"Barbarian" : 0,
+		"Defender" : 0,
+		"Warrior" : 0
+	}
+
+	for card_name in cards:
+		card_count_map[card_name] += 1
+		
 func _process(delta):
 	$ScrollContainer.add_constant_override("margin_bottom", rect_size.y)
 
