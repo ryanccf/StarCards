966f4d6863c0e05366a36c25e94e5a808841ade6
jcf1991 <35503202+jcf1991@users.noreply.github.com>
2022-04-12 22:51:15 -0500

    Made a monster called a Boomjet (title in progress), and added hooks on Field to allow it to spawn explosions. Have not actually tested the monster, nor made a Card and CardListing for it.

--- Field.gd
+++ Field.gd
@@ -2,12 +2,13 @@ extends Area2D
 
 var monsters = []
 var Laser = preload("res://Laser.tscn")
-
+var DirectAttack = preload("res://Actions/DirectAttack.tscn")
 
 func add_monster(monster, position):
 	add_child(monster)
 	monster.set_position(position)
 	monster.connect("spawn_laser", self, "_handle_laser_spawn")
+	monster.connect("boom", self, "_handle_boom")
 	
 func _handle_laser_spawn(laser_position, laser_rotation, laser_target, is_friendly):
 	var laser = Laser.instance()
@@ -17,6 +18,12 @@ func _handle_laser_spawn(laser_position, laser_rotation, laser_target, is_friend
 	laser.set_friendly(is_friendly)
 	laser.connect("target_reached", self, "_clean_up_laser")
 	add_child(laser)
+
+func _handle_boom(position):
+	for _i in 3:
+		var direct_attack = DirectAttack.instance()
+		direct_attack.position = position
+		add_child(direct_attack)
 	
 func _clean_up_laser(laser):
 	pass
---
+++ monsters/Boomjet.gd
@@ -0,0 +1,17 @@
+extends "res://Monster.gd"
+var graphic = load("res://images/enemy_B.png")
+signal boom(position)
+
+func _ready():
+	._ready()
+	update_graphic(graphic)
+	speed = 80
+	laser_max = 12000
+	maxHP = 3
+
+func _check_lasers(delta):
+	if laser_charge <= laser_max:
+		laser_charge += 100 * delta * 80
+	elif $ShootingZone.has_target():
+		laser_charge = float(int(laser_charge) % int(100 * delta * 80))
+		emit_signal("boom", position)
