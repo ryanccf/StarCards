01dd154a4df0745a6b7a1ec1d09e611723ccffc1
jcf1991 <35503202+jcf1991@users.noreply.github.com>
2022-04-17 17:19:47 -0500

    Refactor of Battle. Removed vestigial methods, moved other methods around to be more in order of use.

--- scripts/Battle.gd
+++ scripts/Battle.gd
@@ -16,30 +16,8 @@ func _ready():
 	initialize_player()
 	initialize_opponent()
 
-func _on_PlayerCharacter_turn_over():
-	if ($Background/BackgroundAnchor/Hand.card_count() < 7):
-		draw_card()
-
 func _process(delta):
 	check_battle_end()
-
-func initialize_player():
-	player.increase_speed(25)
-	player.update_graphic(playerOneGraphic)
-	player.position = Vector2(100, 300)
-	player.connect("turn_complete", self, "_on_PlayerCharacter_turn_over")
-	player.connect("death", self, "_clean_up_player")
-	$Background/BackgroundAnchor/Field.add_child(player)
-
-func initialize_opponent():
-	opponent.set_spawn($Background/BackgroundAnchor/Field/SpawnPoint)
-	opponent.set_enemy_base(player)
-	opponent.connect("spawn_monster", $Background/BackgroundAnchor/Field, "add_monster")
-	add_child(opponent)
-
-func assign_card_id():
-	card_id_counter += 1
-	return card_id_counter
 
 func initialize_cards():
 	$Background/BackgroundAnchor/Hand.set_offset(Vector2(105, 0))
@@ -57,6 +35,33 @@ func initialize_cards():
 		$Background/BackgroundAnchor/Deck.add_card(card)
 	$Background/BackgroundAnchor/Deck.shuffle()
 
+func initialize_player():
+	player.increase_speed(25)
+	player.update_graphic(playerOneGraphic)
+	player.position = Vector2(100, 300)
+	player.connect("turn_complete", self, "_on_PlayerCharacter_turn_over")
+	player.connect("death", self, "_clean_up_player")
+	$Background/BackgroundAnchor/Field.add_child(player)
+
+func initialize_opponent():
+	opponent.set_spawn($Background/BackgroundAnchor/Field/SpawnPoint)
+	opponent.set_enemy_base(player)
+	opponent.connect("spawn_monster", $Background/BackgroundAnchor/Field, "add_monster")
+	add_child(opponent)
+
+func check_battle_end():
+	if not opponent.has_base():
+		get_tree().change_scene("res://Victory.tscn")
+	elif game_over:
+		get_tree().change_scene("res://Defeat.tscn")
+
+func _on_PlayerCharacter_turn_over():
+	if ($Background/BackgroundAnchor/Hand.card_count() < 7):
+		draw_card()
+
+func _clean_up_player():
+	game_over = true;
+
 func draw_card():
 	var card = $Background/BackgroundAnchor/Deck.draw_card()
 	if not card:
@@ -72,21 +77,6 @@ func reshuffle():
 		discarded_card.flip()
 		$Background/BackgroundAnchor/Deck.add_card(discarded_card)
 	$Background/BackgroundAnchor/Deck.shuffle()
-
-func discard_selected():
-	$Background/BackgroundAnchor/Discard.add_card($Background/BackgroundAnchor/Hand.pop_selected())
-
-func check_battle_end():
-	if not opponent.has_base():
-		get_tree().change_scene("res://Victory.tscn")
-	elif game_over:
-		get_tree().change_scene("res://Defeat.tscn")
-
-func exit_battle():
-	get_tree().change_scene("res://LevelMap.tscn")
-
-func _clean_up_player():
-	game_over = true;
 
 func _on_DeployZone_input_event(viewport, event, shape_idx):
 	if _is_utility_card_ready(event):
