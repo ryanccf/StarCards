39ba0390b2f3d91723146a96889f7c0083e0f331
jcf1991 <35503202+jcf1991@users.noreply.github.com>
2022-04-27 16:19:51 -0500

    Refactored BattleField, removing Field.

--- scripts/BattleField.gd
+++ scripts/BattleField.gd
@@ -1,4 +1,7 @@
 extends Control
+var Laser = preload("res://Actions/Laser.tscn")
+var DirectAttack = preload("res://Actions/DirectAttack.tscn")
+var monsters = []
 signal deploy_zone_input(event)
 signal zone_of_influence_input(event)
 
@@ -8,18 +11,40 @@ func add_player(player):
 func get_spawn():
 	return $Field/SpawnPoint
 
-func add_monster(monster, position):
-	$Field.add_monster(monster, position)
-
 func add_action(action, event_position):
 	$Field.add_child(action)
 	action.set_position(event_position - $ZoneOfInfluence.global_position)
 
 func monster_from_click(monster, event_position):
-	$Field.add_monster(monster, event_position - $DeployZone.global_position)
+	add_monster(monster, event_position - $DeployZone.global_position)
 
 func _on_DeployZone_input_event(viewport, event, shape_idx):
 	emit_signal("deploy_zone_input", event)
 
 func _on_ZoneOfInfluence_input_event(viewport, event, shape_idx):
 	emit_signal("zone_of_influence_input", event)
+
+func add_monster(monster, position):
+	$Field.add_child(monster)
+	monster.set_position(position)
+	monster.set_bounds($Field/Bounds)
+	monster.connect("spawn_laser", self, "_handle_laser_spawn")
+	monster.connect("boom", self, "_handle_boom")
+	
+func _handle_laser_spawn(laser_position, laser_rotation, laser_target, is_friendly):
+	var laser = Laser.instance()
+	laser.position = laser_position
+	laser.rotation = laser_rotation
+	laser.set_target(laser_target)
+	laser.set_friendly(is_friendly)
+	laser.connect("target_reached", self, "_clean_up_laser")
+	$Field.add_child(laser)
+
+func _handle_boom(position):
+	for _i in 3:
+		var direct_attack = DirectAttack.instance()
+		direct_attack.position = position
+		$Field.add_child(direct_attack)
+
+func _clean_up_laser(laser):
+	pass
