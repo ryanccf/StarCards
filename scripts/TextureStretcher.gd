e49d557938bc32819f93432d598f617a48d3672d
jcf1991 <35503202+jcf1991@users.noreply.github.com>
2022-04-27 17:02:20 -0500

    Made TextureStretcher stretch textures without skewing them.

--- scripts/TextureStretcher.gd
+++ scripts/TextureStretcher.gd
@@ -10,5 +10,6 @@ func _resize_children():
 	for child in get_children():
 		if "texture" in child:
 			var texture_size = child.texture.get_size()
-			child.scale = Vector2(rect_size.x / texture_size.x, rect_size.y / texture_size.y)
+			var modifier = rect_size.x if rect_size.x > rect_size.y else rect_size.y
+			child.scale = Vector2(modifier / texture_size.x, modifier / texture_size.y)
 			child.position = rect_size / 2
