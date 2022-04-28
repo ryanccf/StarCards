287ef18a87df631d13c78407b1f698df7b6a4f5f
jcf1991 <35503202+jcf1991@users.noreply.github.com>
2022-04-28 13:54:06 -0500

    Made ActivityMenu be able to add multiple activities without rendering their buttons on top of one another.

--- scripts/ActivityMenu.gd
+++ scripts/ActivityMenu.gd
@@ -1,6 +1,7 @@
 extends Popup
 var ActivityButton = preload("res://Utilities/ActivityButton.tscn")
 var activities = []
+const BUTTON_SPACING = 20
 
 func add_activity(activity_name, func_ref):
 	activities.push_back({
@@ -9,13 +10,15 @@ func add_activity(activity_name, func_ref):
 	})
 
 func _on_Node2D_about_to_show():
+	var y_offset = 0
 	for activity in activities:
 		var activity_button = ActivityButton.instance()
 		activity_button.set_name(activity.name)
 		activity_button.set_event(activity.event)
 		activity_button.connect("option_selected", self, "_on_option_selected")
 		$Box.add_child(activity_button)
-		activity_button.rect_position = Vector2(-1.5 * activity_button.get_size().x, 0)
+		activity_button.rect_position = Vector2(-1.5 * activity_button.get_size().x, y_offset)
+		y_offset += activity_button.get_size().y * 3 + BUTTON_SPACING
 
 #func click_button():
 #	for activity in $Box.get_children():
