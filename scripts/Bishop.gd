368c5da4b0a5def97438516feb284936cbceff53
jcf1991 <35503202+jcf1991@users.noreply.github.com>
2022-04-30 07:34:49 -0500

    Made bishop move in straight line forward (Not actually desired behavior, just a stepping stone).

--- scripts/Bishop.gd
+++ scripts/Bishop.gd
@@ -4,3 +4,29 @@ var graphic = load("res://images/ship_J.png")
 func _ready():
 	._ready()
 	update_graphic(graphic)
+
+func _physics_process(delta):
+	_check_death()
+	_rotate()
+	if _should_move():
+		_avoid_obstacles()
+		move(delta)
+	_check_lasers(delta)
+	_stabilize_health_bar()
+
+func _rotate():
+	_point_to_locked_target()
+#	look_at(get_target())
+
+func _avoid_obstacles():
+	pass
+#	var path_rotation = $ObstacleAvoider.get_viable_rotation()
+#	if path_rotation != null:
+#		rotation += path_rotation
+
+func move(delta):
+	var collision = $KinematicBody2D.move_and_collide(speed * delta * Vector2(cos(rotation), sin(rotation)))
+	global_position = $KinematicBody2D.global_position
+	$KinematicBody2D.position = Vector2(0, 0)
+	rotation += $KinematicBody2D.rotation 
+	$KinematicBody2D.rotation = 0
