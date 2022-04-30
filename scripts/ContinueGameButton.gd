2d7939d703efd4803942e584b0a4217ed2800397
Ryan <49543952+ryanccf@users.noreply.github.com>
2022-04-29 21:23:19 -0500

    Add Delete Save Game Button
    Add a button to delete a saved game from the main menu.
    
    Spicy!

--- scripts/ContinueGameButton.gd
+++ scripts/ContinueGameButton.gd
@@ -8,3 +8,9 @@ func _on_ContinueGameButton_pressed():
 	Global.set_save_index(save_index)
 	Global.load_game()
 	get_tree().change_scene("res://Screens/LevelMap.tscn")
+
+
+func _on_TextureButton_pressed():
+	Global.set_save_index(save_index)
+	Global.delete_save()
+	queue_free()
