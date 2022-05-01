f5e80b67b05c4631f08430da752f243c6f777a12
jcf1991 <35503202+jcf1991@users.noreply.github.com>
2022-04-30 19:44:44 -0500

    Made bishops bounce off friendly units.

--- scripts/Bishop.gd
+++ scripts/Bishop.gd
@@ -24,11 +24,15 @@ func _physics_process(delta):
 	_check_lasers(delta)
 	_stabilize_health_bar()
 	if out_of_bounds():
-		if position.x > middle:
-			rotation = start_rotation
-		else:
-			rotation = -1 * start_rotation
-		last_rotation = rotation
+		_bounce()
+
+
+func _bounce():
+	if position.x > middle:
+		rotation = start_rotation
+	else:
+		rotation = -1 * start_rotation
+	last_rotation = rotation
 
 func _rotate():
 	_point_to_locked_target()
@@ -42,8 +46,11 @@ func _avoid_obstacles():
 
 func move(delta):
 	var collision = $KinematicBody2D.move_and_collide(speed * delta * Vector2(cos(rotation), sin(rotation)))
+	if collision:
+		_bounce()
 	global_position = $KinematicBody2D.global_position
 	$KinematicBody2D.position = Vector2(0, 0)
 	rotation += $KinematicBody2D.rotation 
 	$KinematicBody2D.rotation = 0
+	
 	
